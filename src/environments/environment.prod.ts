ec38e08e30cf0ed3a8694b249782418680d71d4e
Serhiy Dmytryk <dr.voo1646@gmail.com>
2020-09-23 22:42:02 +0300

    update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  server_user: 'http://nna-api6-qa.eu-central-1.elasticbeanstalk.com/api/'
+  server_user: 'http://nna-api-v2.eu-central-1.elasticbeanstalk.com/api'
 };
