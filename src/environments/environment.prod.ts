5e4e2e50f559e6e5cbc364d572e7df4758c366b0
Serhiy Dmytryk <dr.voo1646@gmail.com>
2020-11-11 16:55:44 +0200

    add route to the load balancer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  server_user:  'http://nna-api-v2.eu-central-1.elasticbeanstalk.com/api/'
+  server_user:  'http://awseb-AWSEB-3JQ1H90JMM3C-696490556.eu-central-1.elb.amazonaws.com/api/'
 };
