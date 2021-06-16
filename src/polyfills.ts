7ea61bb0e4c24ecc3e44fc60833b1ddf60917837
Serhiy Dmytryk <dr.voo1646@gmail.com>
2021-06-17 01:35:26 +0300

    fix npm vulnerabilities

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -57,6 +57,8 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 import '@angular/localize/init';
+import 'src/environments/environment.prod.ts';
+
 
 
 /***************************************************************************************************
