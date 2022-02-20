2a88fb746097b072332cd03ea1c3876c8a67c4e2
Serhiy Dmytryk <dr.voo1646@gmail.com>
2022-02-20 20:14:47 +0200

    fix design of buttong on collections page

--- src/app/layout/dmo-collection/dmo-collection.component.ts
+++ src/app/layout/dmo-collection/dmo-collection.component.ts
@@ -6,7 +6,7 @@ import { CollectionsManagerService } from './../../shared/services/collections-m
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { DmoCollectionDto, DmoCollectionShortDto, AddDmosToCollectionDto,
    DmosIdDto, ShortDmoCollectionDto, ShortDmoDto } from './../models';
-import { Component, OnInit, ViewChild, OnDestroy, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy, ElementRef, ViewChildren, QueryList } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
@@ -17,268 +17,282 @@ import { DmoCollectionsService } from 'src/app/shared/services/dmo-collections.s
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-dmo-collection',
-  templateUrl: './dmo-collection.component.html',
-  styleUrls: ['./dmo-collection.component.scss']
+	selector: 'app-dmo-collection',
+	templateUrl: './dmo-collection.component.html',
+	styleUrls: ['./dmo-collection.component.scss']
 })
 export class DmoCollectionComponent implements OnInit, OnDestroy {
-  currentDmoCollection: DmoCollectionDto;
-  shouldCollectionShowTable = false;
-  awaitingForDmos = false;
-  showPopupOverview = false;
-  showSearchContainer = false;
-  collectionTable: MatTableDataSource<ShortDmoDto>;
-  collectionTableColumn: string[];
-  collectionLength = 0;
-  selectedDmoInCollection: ShortDmoDto;
-  private collectionSubsctiption: Subscription;
-  private removeFromCollectionSub: Subscription;
-  private updateDmosSub: Subscription;
-  private addToCollectionSub: Subscription;
-  private deleteCollectionSub: Subscription;
-  private loadDmosSub: Subscription;
+	currentDmoCollection: DmoCollectionDto;
+	shouldCollectionShowTable = false;
+	awaitingForDmos = false;
+	showPopupOverview = false;
+	showSearchContainer = false;
+	collectionTable: MatTableDataSource<ShortDmoDto>;
+	collectionTableColumn: string[];
+	collectionLength = 0;
+	selectedDmoInCollection: ShortDmoDto;
+	private collectionSubsctiption: Subscription;
+	private removeFromCollectionSub: Subscription;
+	private updateDmosSub: Subscription;
+	private addToCollectionSub: Subscription;
+	private deleteCollectionSub: Subscription;
+	private loadDmosSub: Subscription;
 
-  @ViewChild('collectionPaginator', { static: true }) collectionPaginator: MatPaginator;
-  @ViewChild('collectionSort', { static: true }) collectionSorter: MatSort;
-  @ViewChild('removeFullCollectionModal', { static: true }) removeCollectionModal: NgbActiveModal;
-  @ViewChild('addDmoToCollectionModal', { static: true }) addToCollectionModal: NgbActiveModal;
-  @ViewChild('editCollectionNameField', {static: true}) collectionNameField: ElementRef;
+	@ViewChild('collectionPaginator', { static: true }) collectionPaginator: MatPaginator;
+	@ViewChild('collectionSort', { static: true }) collectionSorter: MatSort;
+	@ViewChild('removeFullCollectionModal', { static: true }) removeCollectionModal: NgbActiveModal;
+	@ViewChild('addDmoToCollectionModal', { static: true }) addToCollectionModal: NgbActiveModal;
+	@ViewChild('editCollectionNameField', {static: true}) collectionNameField: ElementRef;
+	@ViewChildren('dmoActionButtons') dmoActionButtons: QueryList<ElementRef>;
+	
 
-  editCollectionNameForm: FormGroup;
-  get collectionName() { return this.editCollectionNameForm.get('collectionName'); }
-  showEditForm = false;
-  private unsubscribe$: Subject<void> = new Subject();
+	editCollectionNameForm: FormGroup;
+	get collectionName() { return this.editCollectionNameForm.get('collectionName'); }
+	showEditForm = false;
+	private unsubscribe$: Subject<void> = new Subject();
 
-  constructor(
-    private dmoCollectionService: DmoCollectionsService,
-    public matModule: MatDialog,
-    private router: Router,
-    private route: ActivatedRoute,
-    private collectionManager: CollectionsManagerService,
-    private currentSidebarService: CurrentSidebarService) { }
+	constructor(
+		private dmoCollectionService: DmoCollectionsService,
+		public matModule: MatDialog,
+		private router: Router,
+		private route: ActivatedRoute,
+		private collectionManager: CollectionsManagerService,
+		private currentSidebarService: CurrentSidebarService) { }
 
-  ngOnInit() {
-    this.editCollectionNameForm = new FormGroup({
-      'collectionName': new FormControl('', [Validators.required, Validators.maxLength(20)])
-    });
+	ngOnInit() {
+		this.editCollectionNameForm = new FormGroup({
+			'collectionName': new FormControl('', [Validators.required, Validators.maxLength(20)])
+		});
 
-    let collectionId = this.collectionManager.getCurrentCollectionId();
-    if (!collectionId) {
-      collectionId = this.route.snapshot.queryParamMap.get("collectionId");
-      this.collectionManager.setCollectionId(collectionId, true);
-    }
-    if (!collectionId) {
-      this.router.navigate(['/app']);
-    }
+		let collectionId = this.collectionManager.getCurrentCollectionId();
+		if (!collectionId) {
+			collectionId = this.route.snapshot.queryParamMap.get("collectionId");
+			this.collectionManager.setCollectionId(collectionId, true);
+		}
+		if (!collectionId) {
+			this.router.navigate(['/app']);
+		}
 
-    this.collectionSubsctiption = this.collectionManager.currentCollectionObserver.subscribe({next: (_) => this.loadDmos(this.collectionManager.getCurrentCollectionId()) } );
-  }
+		this.collectionSubsctiption = this.collectionManager.currentCollectionObserver.subscribe({next: (_) => this.loadDmos(this.collectionManager.getCurrentCollectionId()) } );
+	}
 
-  ngOnDestroy(): void {
-    this.collectionSubsctiption?.unsubscribe();
-    this.removeFromCollectionSub?.unsubscribe();
-    this.updateDmosSub?.unsubscribe();
-    this.addToCollectionSub?.unsubscribe();
-    this.deleteCollectionSub?.unsubscribe();
-    this.loadDmosSub?.unsubscribe();
-    
-    this.collectionManager.setCollectionId('');
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
+	ngOnDestroy(): void {
+		this.collectionSubsctiption?.unsubscribe();
+		this.removeFromCollectionSub?.unsubscribe();
+		this.updateDmosSub?.unsubscribe();
+		this.addToCollectionSub?.unsubscribe();
+		this.deleteCollectionSub?.unsubscribe();
+		this.loadDmosSub?.unsubscribe();
+		
+		this.collectionManager.setCollectionId('');
+		this.unsubscribe$.next();
+		this.unsubscribe$.complete();
+	}
 
-  onRowSelect(row: ShortDmoDto) {
-    if (this.showEditForm) {
-      return;
-    }
+	onRowSelect(row: ShortDmoDto) {
+		if (this.showEditForm) {
+			return;
+		}
 
-    if (this.selectedDmoInCollection && this.selectedDmoInCollection === row) {
-      this.selectedDmoInCollection = null;
-      return;
-    }
-    this.selectedDmoInCollection = row;
-  }
+		this.dmoActionButtons.toArray().forEach(dabtns => dabtns.nativeElement.classList.remove("action-wrapper-visibility"));
+		let htmlRow = this.dmoActionButtons.toArray().find(dabtns => dabtns.nativeElement.id == row.id)?.nativeElement;
+		if (!htmlRow) {
+			return;
+		}
 
-  redirectToDmo() {
-    if (!this.selectedDmoInCollection) {
-      return;
-    }
-    this.router.navigateByUrl('/app/editor?dmoId=' + this.selectedDmoInCollection.id);
-  }
+		if (this.selectedDmoInCollection && this.selectedDmoInCollection === row) {
+			this.selectedDmoInCollection = null;
+			htmlRow.classList.remove("action-wrapper-visibility");
+			return;
+		}
+		htmlRow.classList.add("action-wrapper-visibility");
+		this.selectedDmoInCollection = row;
+	}
 
-  removeFromCollection() {
-    if (!this.selectedDmoInCollection) {
-      return;
-    }
+	redirectToDmo() {
+		if (!this.selectedDmoInCollection) {
+			return;
+		}
+		this.router.navigateByUrl('/app/editor?dmoId=' + this.selectedDmoInCollection.id);
+	}
 
-    const removeFromCollection$ = this.dmoCollectionService
-      .removeFromCollection(this.selectedDmoInCollection.id, this.currentDmoCollection.id);
+	removeFromCollection() {
+		if (!this.selectedDmoInCollection) {
+			return;
+		}
 
-    const removeAndReload$ = removeFromCollection$.pipe(
-      takeUntil(this.unsubscribe$),
-      map(() => this.loadDmos(this.currentDmoCollection.id)));
+		this.dmoActionButtons.toArray().forEach(dabtns => dabtns.nativeElement.classList.remove("action-wrapper-visibility"));
 
-    this.removeFromCollectionSub = removeAndReload$.subscribe();
-  }
+		const removeFromCollection$ = this.dmoCollectionService
+			.removeFromCollection(this.selectedDmoInCollection.id, this.currentDmoCollection.id);
 
-  onEditCollectionName() {
-    if (this.editCollectionNameForm.valid) {
-      const newCollectionName = this.editCollectionNameForm.get('collectionName').value;
-      if (this.currentDmoCollection.collectionName === newCollectionName) {
-        this.hideEditCollectionNameForm();
-        return;
-      }
+		const removeAndReload$ = removeFromCollection$.pipe(
+			takeUntil(this.unsubscribe$),
+			map(() => this.loadDmos(this.currentDmoCollection.id)));
 
-      const collectionId = this.collectionManager.getCurrentCollectionId();
-      const updateCollectionName$ = this.dmoCollectionService.updateCollectionName(collectionId, newCollectionName);
-      const getCollectionName$ = this.dmoCollectionService.getCollectionName(collectionId);
+		this.removeFromCollectionSub = removeAndReload$.subscribe();
+	}
 
-      const updateAndGet$ =
-        updateCollectionName$.pipe(
-          takeUntil(this.unsubscribe$),
-          finalize(() => this.hideEditCollectionNameForm()),
-          concatMap(() => getCollectionName$.pipe(
-            takeUntil(this.unsubscribe$),
-            map((response: DmoCollectionShortDto) => {
-              this.currentDmoCollection.collectionName = response.collectionName;
-            }))
-          ));
+	onEditCollectionName() {
+		if (this.editCollectionNameForm.valid) {
+			const newCollectionName = this.editCollectionNameForm.get('collectionName').value;
+			if (this.currentDmoCollection.collectionName === newCollectionName) {
+				this.hideEditCollectionNameForm();
+				return;
+			}
 
-      this.updateDmosSub = updateAndGet$.subscribe();
-    }
-  }
+			const collectionId = this.collectionManager.getCurrentCollectionId();
+			const updateCollectionName$ = this.dmoCollectionService.updateCollectionName(collectionId, newCollectionName);
+			const getCollectionName$ = this.dmoCollectionService.getCollectionName(collectionId);
 
-  async onAddDmo() {
-    this.awaitingForDmos = true;
-    this.showPopupOverview = true;
-    const collectionWithDmo = new ShortDmoCollectionDto();
-    collectionWithDmo.collectionName = this.currentDmoCollection.collectionName;
+			const updateAndGet$ =
+				updateCollectionName$.pipe(
+					takeUntil(this.unsubscribe$),
+					finalize(() => this.hideEditCollectionNameForm()),
+					concatMap(() => getCollectionName$.pipe(
+						takeUntil(this.unsubscribe$),
+						map((response: DmoCollectionShortDto) => {
+							this.currentDmoCollection.collectionName = response.collectionName;
+						})
+					))
+				);
 
-    const collectionId = this.collectionManager.getCurrentCollectionId();
-    await this.dmoCollectionService.getExcludedDmos(collectionId).toPromise()
-      .then((dmos: ShortDmoDto[]) => collectionWithDmo.dmos = 
-        dmos.map(d => { 
-          let dmo = new ShortDmoDto(d.name, d.movieTitle);
-          dmo.id = d.id;
-          dmo.shortComment = d.shortComment;
-          return dmo;
-        })
-      );
+			this.updateDmosSub = updateAndGet$.subscribe();
+		}
+	}
 
-    this.awaitingForDmos = false;
-    const dialogRef = this.matModule.open(AddDmosPopupComponent, {
-      data: collectionWithDmo,
-      minWidth: '430px'
-    });
+	async onAddDmo() {
+		this.awaitingForDmos = true;
+		this.showPopupOverview = true;
+		const collectionWithDmo = new ShortDmoCollectionDto();
+		collectionWithDmo.collectionName = this.currentDmoCollection.collectionName;
 
-    dialogRef.afterClosed().subscribe({
-      next: (selectDmos: ShortDmoDto[]) => {
-        this.showPopupOverview = false;
-        if (!selectDmos) {
-          return;
-        }
+		const collectionId = this.collectionManager.getCurrentCollectionId();
+		await this.dmoCollectionService.getExcludedDmos(collectionId).toPromise()
+			.then((dmos: ShortDmoDto[]) => collectionWithDmo.dmos = 
+				dmos.map(d => { 
+					let dmo = new ShortDmoDto(d.name, d.movieTitle);
+					dmo.id = d.id;
+					dmo.shortComment = d.shortComment;
+					return dmo;
+				})
+			);
 
-        const dto = new AddDmosToCollectionDto();
-        dto.collectionId = collectionId;
-        dto.dmos = selectDmos.map(d => new DmosIdDto(d.id));
+		this.awaitingForDmos = false;
+		const dialogRef = this.matModule.open(AddDmosPopupComponent, {
+			data: collectionWithDmo,
+			minWidth: '430px'
+		});
 
-        const addToCollection$ = this.dmoCollectionService.addDmosToCollection(dto);
-        addToCollection$.pipe(takeUntil(this.unsubscribe$));
+		dialogRef.afterClosed().subscribe({
+			next: (selectDmos: ShortDmoDto[]) => {
+				this.showPopupOverview = false;
+				if (!selectDmos) {
+					return;
+				}
 
-        this.addToCollectionSub = addToCollection$.subscribe({
-          next: () => this.loadDmos(this.currentDmoCollection.id)
-        });
-      }
-    });
-  }
+				const dto = new AddDmosToCollectionDto();
+				dto.collectionId = collectionId;
+				dto.dmos = selectDmos.map(d => new DmosIdDto(d.id));
 
-  onRemoveCollection() {
-    const delteCollectionModal = this.matModule.open(RemoveCollectionPopupComponent, {
-      data: this.currentDmoCollection.collectionName
-    });
+				const addToCollection$ = this.dmoCollectionService.addDmosToCollection(dto);
+				addToCollection$.pipe(takeUntil(this.unsubscribe$));
 
-    delteCollectionModal.afterClosed().subscribe({
-      next: (shouldDelete: boolean) => {
-        if (!shouldDelete) {
-          return;
-        }
-        const deleteAndRedirect$ = this.dmoCollectionService.deleteCollection(this.currentDmoCollection.id);
+				this.addToCollectionSub = addToCollection$.subscribe({
+					next: () => this.loadDmos(this.currentDmoCollection.id)
+				});
+			}
+		});
+	}
 
-        this.deleteCollectionSub = deleteAndRedirect$.subscribe({
-          next: () => { this.redirectAfterRemove(); }
-        });
-      }
-    });
-  }
+	onRemoveCollection() {
+		const delteCollectionModal = this.matModule.open(RemoveCollectionPopupComponent, {
+			data: this.currentDmoCollection.collectionName
+		});
 
-  hideEditCollectionNameForm() {
-    this.editCollectionNameForm.reset();
-    this.showEditForm = false;
-  }
+		delteCollectionModal.afterClosed().subscribe({
+			next: (shouldDelete: boolean) => {
+				if (!shouldDelete) {
+					return;
+				}
+				const deleteAndRedirect$ = this.dmoCollectionService.deleteCollection(this.currentDmoCollection.id);
 
-  showEditCollectionNameForm() {
-    this.editCollectionNameForm.get('collectionName').setValue(this.currentDmoCollection.collectionName);
-    
-    this.showEditForm = true;
-    this.selectedDmoInCollection = null;
-    
-    setTimeout(() => {
-      this.collectionNameField.nativeElement.focus();
-    }, 100);
-  }
+				this.deleteCollectionSub = deleteAndRedirect$.subscribe({
+					next: () => { this.redirectAfterRemove(); }
+				});
+			}
+		});
+	}
 
-  applyCollectionFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.collectionTable.filter = filterValue.trim().toLowerCase();
+	hideEditCollectionNameForm() {
+		this.editCollectionNameForm.reset();
+		this.showEditForm = false;
+	}
 
-    if (this.collectionTable.paginator) {
-      this.collectionTable.paginator.firstPage();
-    }
-  }
+	showEditCollectionNameForm() {
+		this.editCollectionNameForm.get('collectionName').setValue(this.currentDmoCollection.collectionName);
+		this.dmoActionButtons.toArray().forEach(dabtns => dabtns.nativeElement.classList.remove("action-wrapper-visibility"));
+		
+		this.showEditForm = true;
+		this.selectedDmoInCollection = null;
+		
+		setTimeout(() => {
+			this.collectionNameField.nativeElement.focus();
+		}, 100);
+	}
 
-  resetSelected() {
-    this.selectedDmoInCollection = null;
-  }
+	applyCollectionFilter(event: Event) {
+		const filterValue = (event.target as HTMLInputElement).value;
+		this.collectionTable.filter = filterValue.trim().toLowerCase();
 
-  toggleSearchContainer(event: any) {
-    (event.target as HTMLInputElement).value = '';
-    this.showSearchContainer = !this.showSearchContainer;
-  }
+		if (this.collectionTable.paginator) {
+			this.collectionTable.paginator.firstPage();
+		}
+	}
 
-  private redirectAfterRemove() {
-    this.currentSidebarService.setMenu(null);
-    this.router.navigateByUrl('/app');
-  }
+	resetSelected() {
+		this.selectedDmoInCollection = null;
+	}
 
-  private loadDmos(collectionId: string) {
-    this.resetCollectionTable();
-    this.loadDmosSub = this.dmoCollectionService.getWithDmos(collectionId)
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe({
-        next: (response: DmoCollectionDto) => {
-          this.currentDmoCollection = response;
-          this.initializeCollectionTable(this.currentDmoCollection.dmos);
-        }
-      });
-  }
+	toggleSearchContainer(event: any) {
+		(event.target as HTMLInputElement).value = '';
+		this.showSearchContainer = !this.showSearchContainer;
+	}
 
-  private resetCollectionTable() {
-    this.currentDmoCollection = null;
-    this.shouldCollectionShowTable = false;
-    this.collectionTable = null;
-    this.collectionTableColumn = null;
-    this.collectionLength = 0;
-    this.selectedDmoInCollection = null;
-    this.hideEditCollectionNameForm();
-    this.showEditForm = false;
-  }
+	private redirectAfterRemove() {
+		this.currentSidebarService.setMenu(null);
+		this.router.navigateByUrl('/app');
+	}
 
-  private initializeCollectionTable(dataSource: ShortDmoDto[]) {
-    this.collectionTableColumn = ['name', 'movieTitle', 'dmoStatus', 'shortComment', 'mark'];
-    this.collectionTable = new MatTableDataSource(dataSource);
-    this.collectionTable.paginator = this.collectionPaginator;
-    this.collectionTable.sort = this.collectionSorter;
-    this.collectionLength = this.currentDmoCollection.dmos.length;
-    this.shouldCollectionShowTable = true;
-  }
+	private loadDmos(collectionId: string) {
+		this.resetCollectionTable();
+		this.loadDmosSub = this.dmoCollectionService.getWithDmos(collectionId)
+			.pipe(takeUntil(this.unsubscribe$))
+			.subscribe({
+				next: (response: DmoCollectionDto) => {
+					this.currentDmoCollection = response;
+					this.initializeCollectionTable(this.currentDmoCollection.dmos);
+				}
+			});
+	}
+
+	private resetCollectionTable() {
+		this.currentDmoCollection = null;
+		this.shouldCollectionShowTable = false;
+		this.collectionTable = null;
+		this.collectionTableColumn = null;
+		this.collectionLength = 0;
+		this.selectedDmoInCollection = null;
+		this.hideEditCollectionNameForm();
+		this.showEditForm = false;
+	}
+
+	private initializeCollectionTable(dataSource: ShortDmoDto[]) {
+		this.collectionTableColumn = ['name', 'movieTitle', 'dmoStatus', 'shortComment', 'actions']; //'mark'
+		this.collectionTable = new MatTableDataSource(dataSource);
+		this.collectionTable.paginator = this.collectionPaginator;
+		this.collectionTable.sort = this.collectionSorter;
+		this.collectionLength = this.currentDmoCollection.dmos.length;
+		this.shouldCollectionShowTable = true;
+	}
 }
