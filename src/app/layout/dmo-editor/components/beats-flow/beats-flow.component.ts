453c6ed7041c3570596dae030a2f1ecc38eac22b
Serhiy Dmytryk <dr.voo1646@gmail.com>
2022-04-19 23:30:57 +0300

    minor refactor of editor code design fix caret offset when innerText contains html tags

--- src/app/layout/dmo-editor/components/beats-flow/beats-flow.component.ts
+++ src/app/layout/dmo-editor/components/beats-flow/beats-flow.component.ts
@@ -66,10 +66,6 @@ export class BeatsFlowComponent implements AfterViewInit  {
 
   	// #region general settings
 
-	preventDrag(event: any) {
-		event.preventDefault();
-	}
-
 	private setupSubscription() {
 		this.updateBeatsEvent.subscribe(update => {
 		this.beats = [...update.beats]
@@ -148,241 +144,8 @@ export class BeatsFlowComponent implements AfterViewInit  {
   	// #endregion
 
 
-   // #region beat data holders
-
-	beatContainerClick($event: any): void {
-		if ($event.target.className == 'beat-data-holder-container') {
-			$event.target.children[0].focus();
-			this.shiftCursorToTheEndOfChildren($event.target);
-		}
-	}
-
-	setBeatKeyMetaData($event: any): void {
-		let key = $event.which || $event.keyCode || $event.charCode;
-
-		if (key == 17) { // control
-			this.controlIsPressed = true;
-		}
-
-		if (key == 16) { // shift
-			this.shiftIsPressed = true;
-		} 
-
-		if (key == 9) { // tab
-			this.tabIsPressed = true;
-			$event.preventDefault();
-		}
-
-		if (key == 37 || key == 38 || key == 39 || key == 40) { // arrow keys
-			this.focusNextPreviousBeat(key, $event);
-			return;
-		}
-
-		if (key == 13) { // enter
-			if (this.shiftIsPressed == false) {
-				$event.preventDefault();
-				this.addBeat.emit({ beatIdFrom: this.selectBeatIdFromBeatDataHolder($event.target) });
-			} 
-			return;
-		}
-
-		if (key == 8 || key == 46) { // delete or backspace
-			this.valueBeforeRemove = $event.target.innerHTML;
-		}
-
-		this.onDownLines = this.calculateLineCount($event.target);
-	}
-
-	setBeatValue($event: any): void {
-		let key = $event.which || $event.keyCode || $event.charCode;
-
-		if (key == 17) { // control
-			this.controlIsPressed = false;
-		}
-
-		if (key == 16) { // shift
-			this.shiftIsPressed = false;
-		}
-
-		if (key == 9) { // tab
-			this.tabIsPressed = false;
-			$event.preventDefault();
-		}
-
-		if (key == 37 || key == 38 || key == 39 || key == 40) { // arrow keys
-			return;
-		}
-
-		if (key == 13) { // enter
-			if (!this.shiftIsPressed) {
-				$event.preventDefault();
-				return;
-			}
-		}
-
-		if (key == 8 || key == 46) { // delete or backspace
-			let text = $event.target.innerHTML;
-			if (text.replace(/\s+/g, '').length == 0 && this.beatsIds.length != 1) {
-				if (this.valueBeforeRemove == '') {
-					this.removeBeat.emit({beatIdToRemove: this.selectBeatIdFromBeatDataHolder($event.target)});
-					return;
-				}
-			}
-		}
-
-		this.onUpLines = this.calculateLineCount($event.target);
-
-		this.checkLineCounts($event.target);
-	}
-
-	finalizeBeat($event: any): void {
-		if ($event.relatedTarget == null) {
-			this.syncBeats.emit('beat');
-		} else {
-			let timePickerId = this.selectBeatIdFromTimePicker($event.relatedTarget);
-			let beatId = this.selectBeatIdFromBeatDataHolder($event.target);
-
-			if (beatId != timePickerId) {
-				this.syncBeats.emit('beat');
-			}
-		}
-	}
-
-	beatChanged($event: any, index: number): void {
-		this.beatsMetaData[index].isDirty = true;
-	}
-
-	private focusNextPreviousBeat(key: number, $event: any): void {
-		if (key == 38) { // up
-			if (this.tabIsPressed) {
-				this.beatsIds.forEach((beatId, i) => {
-					if (beatId == this.selectBeatIdFromBeatDataHolder($event.target)) {
-						if (i != 0) {
-							$event.preventDefault();
-							this.shiftCursorToTheEndOfChildren(this.beatDataHolderElements.toArray()[i - 1].nativeElement.parentElement);
-							return;
-						}
-					}
-				});
-			}
-			return;
-		} else if (key == 40 || key == 39) { // down or right
-			if (this.tabIsPressed) {
-				this.beatsIds.forEach((beatId, i) => {
-					if (beatId == this.selectBeatIdFromBeatDataHolder($event.target)) {
-						if (i != this.beatsIds.length - 1) {
-							$event.preventDefault();
-							this.shiftCursorToTheEndOfChildren(this.beatDataHolderElements.toArray()[i + 1].nativeElement.parentElement);
-							return;
-						}
-					}
-				});
-			}
-		return;
-		} else if (key == 37) { // left
-			if (this.tabIsPressed || document.getSelection().focusOffset == 0) {
-				this.beatsIds.forEach((beatId, i) => {
-					if (beatId == this.selectBeatIdFromBeatDataHolder($event.target)) {
-						let timePickerElement = this.timePickersElements.toArray()[i].nativeElement;
-						timePickerElement.focus();
-						if (timePickerElement.value == this.defaultEmptyTimePickerValue) {
-							timePickerElement.setSelectionRange(0,0);
-						} else {
-							timePickerElement.setSelectionRange(8,8);
-							setTimeout(() => {timePickerElement.setSelectionRange(8,8); }, 10);
-						}
-						return;
-					}
-				})
-			}
-		}
-	}
-
-	private checkLineCounts(element: any) {
-		let lastUp = this.onUpLines;
-		let lastDown = this.onDownLines;
-
-		if (lastDown.lines < lastUp.lines ) {
-			this.lineCountChanged.emit({beatId: this.selectBeatIdFromBeatDataHolder(element), newLineCount: lastUp});
-		} else if (lastDown.lines > lastUp.lines) {
-			this.lineCountChanged.emit({beatId: this.selectBeatIdFromBeatDataHolder(element), newLineCount: lastUp});
-		}
-	}
-
-	private setupLastBeatMargin() {
-		this.beatDataHolderElements.last.nativeElement.parentNode.style.marginBottom = '0px';
-	}
-
-	private setupBeatMargin($event: any, calculatedLines: any): void {
-		if (calculatedLines.lines % 2 != 0) {
-			if ($event.target.parentNode.style.marginBottom == '0px' && calculatedLines.lines != 1) {
-				let margin = (this.beatContrainerMinHeight * calculatedLines.newLineCount) - (calculatedLines.lines * this.beatLineHeigth);
-				$event.target.parentNode.style.marginBottom = `${margin}px`;
-			}
-		} else {
-			$event.target.parentNode.style.marginBottom = '0px';
-		}
-	}
-
-	private calculateLineCount(nativeElement: any): any {
-		let spanHeight = nativeElement.offsetHeight;
-		let lines = Math.ceil(spanHeight / this.beatLineHeigth);
-
-		return lines <= 1
-			? { lineCount: 1, lines: lines }
-			: { lineCount: lines % 2 == 0 ? (lines / 2) : Math.floor(lines / 2) + 1, lines: lines};
-	}
-
-	private setupBeatDataHolderValuesAndMetaData(): void {
-		this.beatsMetaData = [];
-		this.beatsIds = [];
-
-		this.beatDataHolderElements.forEach((beatDataHolder) => {
-			let beat = this.beats.find(b => b.beatId == this.selectBeatIdFromBeatDataHolder(beatDataHolder.nativeElement));
-			if (!beat) {
-				return;
-			}
-
-			beatDataHolder.nativeElement.innerHTML = beat.text;
-			this.beatsMetaData.push(this.calculateLineCount(beatDataHolder.nativeElement));
-			this.beatsIds.push(beat.beatId);
-		});
-	}
-
-	private selectBeatIdFromBeatDataHolder(beatHolder: any): string {
-		let beatSufix = 'beat_';
-		let id = beatHolder.getAttribute('id');
-		if (!id) {
-			return null;
-		}
-		return id.substring(beatSufix.length);
-	}
-
-	private shiftCursorToTheEndOfChildren(dataHolderContainer: any) {
-		if (!dataHolderContainer.children) {
-			return;
-		}
-
-		var range = document.createRange();
-		var selection = window.getSelection();
-		let dataHolder = dataHolderContainer.lastChild;
-
-		if (!dataHolder.lastChild) {
-			range.setStart(dataHolder, 0);
-		} else {
-			range.setStart(dataHolder.lastChild, dataHolder.lastChild.textContent.length);
-		}
-
-		range.collapse(true)
-		selection.removeAllRanges();
-		selection.addRange(range);
-	}
-
-    // #endregion
-
-
-  	// #region time picker
-
+	// #region key event handlers
+	
 	setTimePickerKeyMetaData(event: any): void {
 		let key = event.which || event.keyCode || event.charCode;
 		if (((key < 48 || key > 59) &&  // numbers
@@ -522,6 +285,278 @@ export class BeatsFlowComponent implements AfterViewInit  {
 			event.target.setSelectionRange(8, 8);
 		}
 	}
+
+	beatContainerClick($event: any): void {
+		if ($event.target.className == 'beat-data-holder-container') {
+			$event.target.children[0].focus();
+			this.shiftCursorToTheEndOfChildren($event.target);
+		}
+	}
+
+	setBeatKeyMetaData($event: any): void {
+		let key = $event.which || $event.keyCode || $event.charCode;
+
+		if (key == 17) { // control
+			this.controlIsPressed = true;
+		}
+
+		if (key == 16) { // shift
+			this.shiftIsPressed = true;
+		} 
+
+		if (key == 9) { // tab
+			this.tabIsPressed = true;
+			$event.preventDefault();
+		}
+
+		if (key == 37 || key == 38 || key == 39 || key == 40) { // arrow keys
+			this.focusNextPreviousBeat(key, $event);
+			return;
+		}
+
+		if (key == 13) { // enter
+			if (this.shiftIsPressed == false) {
+				$event.preventDefault();
+				this.addBeat.emit({ beatIdFrom: this.selectBeatIdFromBeatDataHolder($event.target) });
+			} 
+			return;
+		}
+
+		if (key == 8 || key == 46) { // delete or backspace
+			this.valueBeforeRemove = $event.target.innerHTML;
+		}
+
+		this.onDownLines = this.calculateLineCount($event.target);
+	}
+
+	setBeatValue($event: any): void {
+		let key = $event.which || $event.keyCode || $event.charCode;
+
+		if (key == 17) { // control
+			this.controlIsPressed = false;
+		}
+
+		if (key == 16) { // shift
+			this.shiftIsPressed = false;
+		}
+
+		if (key == 9) { // tab
+			this.tabIsPressed = false;
+			$event.preventDefault();
+		}
+
+		if (key == 37 || key == 38 || key == 39 || key == 40) { // arrow keys
+			return;
+		}
+
+		if (key == 13) { // enter
+			if (!this.shiftIsPressed) {
+				$event.preventDefault();
+				return;
+			}
+		}
+
+		if (key == 8 || key == 46) { // delete or backspace
+			let text = $event.target.innerHTML;
+			if (text.replace(/\s+/g, '').length == 0 && this.beatsIds.length != 1) {
+				if (this.valueBeforeRemove == '') {
+					this.removeBeat.emit({beatIdToRemove: this.selectBeatIdFromBeatDataHolder($event.target)});
+					return;
+				}
+			}
+		}
+
+		this.onUpLines = this.calculateLineCount($event.target);
+
+		this.checkLineCounts($event.target);
+	}
+
+	finalizeBeat($event: any): void {
+		this.clearInnerTagsIfBeatIsEmpty($event);
+		if ($event.relatedTarget == null) {
+			this.syncBeats.emit('beat');
+		} else {
+			let timePickerId = this.selectBeatIdFromTimePicker($event.relatedTarget);
+			let beatId = this.selectBeatIdFromBeatDataHolder($event.target);
+
+			if (beatId != timePickerId) {
+				this.syncBeats.emit('beat');
+			}
+		}
+	}
+
+	beatChanged($event: any, index: number): void {
+		this.beatsMetaData[index].isDirty = true;
+	}
+
+	preventDrag(event: any) {
+		event.preventDefault();
+	}
+
+	// #endregion
+
+
+    // #region beat data holders (helpers)
+
+
+	private clearInnerTagsIfBeatIsEmpty($event: any): void {
+		console.log($event);
+		console.log($event.target.childNodes);
+		if (!$event.target.lastChild) {
+			return;
+		}
+
+		if ($event.target.childNodes.length == 1) {
+			if ($event.target.lastChild.nodeType == 3) {
+				const pureText = $event.target.innerText.replace(/ +/g, '');
+				if (pureText == '') {
+					$event.target.innerText = '';
+					return;
+				}
+			}
+		}
+
+		let childrenWithText = [...$event.target.childNodes].filter(child => child.nodeType == 3 && child.textContent.replace(/ +/g, '') != '');
+		if (childrenWithText.length == 0) {
+			$event.target.innerText = '';
+		}
+	}
+
+	private focusNextPreviousBeat(key: number, $event: any): void {
+		if (key == 38) { // up
+			if (this.tabIsPressed) {
+				this.beatsIds.forEach((beatId, i) => {
+					if (beatId == this.selectBeatIdFromBeatDataHolder($event.target)) {
+						if (i != 0) {
+							$event.preventDefault();
+							this.shiftCursorToTheEndOfChildren(this.beatDataHolderElements.toArray()[i - 1].nativeElement.parentElement);
+							return;
+						}
+					}
+				});
+			}
+			return;
+		} else if (key == 40 || key == 39) { // down or right
+			if (this.tabIsPressed) {
+				this.beatsIds.forEach((beatId, i) => {
+					if (beatId == this.selectBeatIdFromBeatDataHolder($event.target)) {
+						if (i != this.beatsIds.length - 1) {
+							$event.preventDefault();
+							this.shiftCursorToTheEndOfChildren(this.beatDataHolderElements.toArray()[i + 1].nativeElement.parentElement);
+							return;
+						}
+					}
+				});
+			}
+			return;
+		} else if (key == 37) { // left
+			if (this.tabIsPressed || document.getSelection().focusOffset == 0) {
+				this.beatsIds.forEach((beatId, i) => {
+					if (beatId == this.selectBeatIdFromBeatDataHolder($event.target)) {
+						let timePickerElement = this.timePickersElements.toArray()[i].nativeElement;
+						timePickerElement.focus();
+						if (timePickerElement.value == this.defaultEmptyTimePickerValue) {
+							timePickerElement.setSelectionRange(0,0);
+						} else {
+							timePickerElement.setSelectionRange(8,8);
+							setTimeout(() => {timePickerElement.setSelectionRange(8,8); }, 10);
+						}
+						return;
+					}
+				})
+			}
+		}
+	}
+
+	private checkLineCounts(element: any) {
+		let lastUp = this.onUpLines;
+		let lastDown = this.onDownLines;
+
+		if (lastDown.lines < lastUp.lines ) {
+			this.lineCountChanged.emit({beatId: this.selectBeatIdFromBeatDataHolder(element), newLineCount: lastUp});
+		} else if (lastDown.lines > lastUp.lines) {
+			this.lineCountChanged.emit({beatId: this.selectBeatIdFromBeatDataHolder(element), newLineCount: lastUp});
+		}
+	}
+
+	private setupLastBeatMargin() {
+		this.beatDataHolderElements.last.nativeElement.parentNode.style.marginBottom = '0px';
+	}
+
+	private setupBeatMargin($event: any, calculatedLines: any): void {
+		if (calculatedLines.lines % 2 != 0) {
+			if ($event.target.parentNode.style.marginBottom == '0px' && calculatedLines.lines != 1) {
+				let margin = (this.beatContrainerMinHeight * calculatedLines.newLineCount) - (calculatedLines.lines * this.beatLineHeigth);
+				$event.target.parentNode.style.marginBottom = `${margin}px`;
+			}
+		} else {
+			$event.target.parentNode.style.marginBottom = '0px';
+		}
+	}
+
+	private calculateLineCount(nativeElement: any): any {
+		let spanHeight = nativeElement.offsetHeight;
+		let lines = Math.ceil(spanHeight / this.beatLineHeigth);
+
+		return lines <= 1
+			? { lineCount: 1, lines: lines }
+			: { lineCount: lines % 2 == 0 ? (lines / 2) : Math.floor(lines / 2) + 1, lines: lines};
+	}
+
+	private setupBeatDataHolderValuesAndMetaData(): void {
+		this.beatsMetaData = [];
+		this.beatsIds = [];
+
+		this.beatDataHolderElements.forEach((beatDataHolder) => {
+			let beat = this.beats.find(b => b.beatId == this.selectBeatIdFromBeatDataHolder(beatDataHolder.nativeElement));
+			if (!beat) {
+				return;
+			}
+
+			beatDataHolder.nativeElement.innerHTML = beat.text;
+			this.beatsMetaData.push(this.calculateLineCount(beatDataHolder.nativeElement));
+			this.beatsIds.push(beat.beatId);
+		});
+	}
+
+	private selectBeatIdFromBeatDataHolder(beatHolder: any): string {
+		let beatSufix = 'beat_';
+		let id = beatHolder.getAttribute('id');
+		if (!id) {
+			return null;
+		}
+		return id.substring(beatSufix.length);
+	}
+
+	private shiftCursorToTheEndOfChildren(dataHolderContainer: any) {
+		if (!dataHolderContainer.children) {
+			return;
+		}
+
+		let range = document.createRange();
+		let dataHolder = dataHolderContainer.lastChild;
+
+		if (!dataHolder.lastChild) {
+			dataHolder.focus();
+			return;
+		} else {
+			if (dataHolder.lastChild.nodeType == 3) { // TEXT_NODE
+				range.setStart(dataHolder.lastChild, dataHolder.lastChild.textContent.length);
+			} else { // any other element
+				range.setStart(dataHolder.lastChild.lastChild, dataHolder.lastChild.lastChild.textContent.length);
+			}
+
+			range.collapse(true)
+			let selection = window.getSelection();
+			selection.removeAllRanges();
+			selection.addRange(range);
+		}
+	}
+
+    // #endregion
+
+
+  	// #region time picker (helpers)
 
 	private setupTimePickerValues(): void {
 		this.timePickersElements.forEach((picker) => {
