1b8cc50b66242b54ba49aa860a201d9c100d56f9
Serhii Dmytryk <dr.voo1646@gmail.com>
2023-03-29 22:30:06 +0300

    switch back to default domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
 	production: true,
-	server_user: 'https://dmo-editor.com/api/',
+	// server_user: 'https://dmo-editor.com/api/',
+	server_user: 'https://nna-dev-api-v6.azurewebsites.net/api/',
 	google_client_id: "778642260315-mu3c3t4805u3qsl8ok0jm7760mbatn7s.apps.googleusercontent.com"
 };
